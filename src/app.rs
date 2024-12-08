83e5e83b0de475172eb78de4c9f38de9687cfb4b
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-08 12:06:16 +0100

    moved app to own mod

---
+++ src/app.rs
@@ -0,0 +1,40 @@
+use input::handle_key_event;
+use winit::application::ApplicationHandler;
+use winit::dpi::LogicalSize;
+use winit::event::WindowEvent;
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowAttributes, WindowId};
+
+mod input;
+
+#[derive(Default)]
+pub struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = WindowAttributes::default()
+            .with_title("Pong")
+            .with_inner_size(LogicalSize::new(800, 600))
+            .with_resizable(false);
+        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::KeyboardInput {
+                event,
+                is_synthetic: false,
+                ..
+            } => handle_key_event(event),
+            _ => (),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,8 @@
-use input::handle_key_event;
-use winit::application::ApplicationHandler;
-use winit::dpi::LogicalSize;
-use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowAttributes, WindowId};
+use app::App;
+use winit::event_loop::{ControlFlow, EventLoop};
 
+mod app;
 mod context;
-mod input;
-
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window_attributes = WindowAttributes::default()
-            .with_title("Pong")
-            .with_inner_size(LogicalSize::new(800, 600))
-            .with_resizable(false);
-        self.window = Some(event_loop.create_window(window_attributes).unwrap());
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        _window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::KeyboardInput {
-                event,
-                is_synthetic: false,
-                ..
-            } => handle_key_event(event),
-            _ => (),
-        }
-    }
-}
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
