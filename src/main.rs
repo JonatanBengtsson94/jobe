e5f1562278e0506335105f018532039b1e08197c
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-09 22:19:57 +0100

    added bindgroup layout builder

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ mod context;
 mod input;
 mod renderer_backend;
 
+// TODO::
+// Bindgroup layout builder - Describe the shape of data going into shader
+// Make sure pieline builder can take bindgroup layout
+// Construct bindgroup layout - Use it in pipeline
+// Bindgroup builder
+// Engine has texture
+// Shader uses texture
+
 fn main() {
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
