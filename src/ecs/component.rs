dd76b0aa7884270b74e6a505573d662369a323b5
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-12 21:18:51 +0100

    added component manager

--- src/ecs/component.rs
+++ src/ecs/component.rs
@@ -1,4 +1,5 @@
 use super::types::Entity;
+use std::any::Any;
 use std::{any::TypeId, collections::HashMap, usize};
 
 pub struct ComponentArray<T> {
@@ -11,9 +12,7 @@ impl<T> ComponentArray<T> {
             components: Vec::new(),
         }
     }
-}
 
-impl<T> ComponentArray<T> {
     pub fn insert(&mut self, entity: Entity, component: T) {
         if entity as usize > self.components.len() {
             self.components.resize_with(entity as usize + 1, || None);
@@ -23,5 +22,27 @@ impl<T> ComponentArray<T> {
 }
 
 pub struct ComponentManager {
-    component_arrays: HashMap<TypeId, Box<dyn std::any::Any>>,
+    component_arrays: HashMap<TypeId, Box<dyn Any>>,
+}
+
+impl ComponentManager {
+    pub fn new() -> Self {
+        ComponentManager {
+            component_arrays: HashMap::new(),
+        }
+    }
+
+    pub fn add_component<T: 'static>(&mut self, entity: Entity, component: T) {
+        let type_id = TypeId::of::<T>();
+        let array = self
+            .component_arrays
+            .entry(type_id)
+            .or_insert_with(|| Box::new(ComponentArray::<T>::new()) as Box<dyn Any>);
+
+        if let Some(array) = array.downcast_mut::<ComponentArray<T>>() {
+            array.insert(entity, component);
+        } else {
+            panic!("Failed to downcast to correct component array type.");
+        }
+    }
 }
