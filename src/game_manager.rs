bfe236996f4c13fa1b66ec6dd98b975622a28316
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-13 19:22:54 +0100

    added system manager

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -1,15 +1,29 @@
-use crate::ecs::{component::ComponentManager, entity::EntityManager};
+use crate::ecs::{
+    component::{Component, ComponentManager},
+    entity::{Entity, EntityManager},
+    system::SystemManager,
+};
 
 pub struct GameManager {
-    entitymanager: EntityManager,
-    componentmanager: ComponentManager,
+    entity_manager: EntityManager,
+    component_manager: ComponentManager,
+    system_manager: SystemManager,
 }
 
 impl GameManager {
     pub fn new() -> Self {
         GameManager {
-            entitymanager: EntityManager::new(),
-            componentmanager: ComponentManager::new(),
+            entity_manager: EntityManager::new(),
+            component_manager: ComponentManager::new(),
+            system_manager: SystemManager::new(),
         }
+    }
+
+    pub fn add_component(&mut self, entity: Entity, component: Component) {
+        self.component_manager.add_component(entity, component);
+        let signature = self.entity_manager.get_signature(entity);
+        // Set signature bits based on componentid
+        // entitymanager.setsignature
+        // Tell system manager?
     }
 }
