c1dc899254c7e02f82c3783cfb7f52f75108f514
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-14 21:27:21 +0100

    moved component manager logic to manager

--- src/ecs/component.rs
+++ src/ecs/component.rs
@@ -1,9 +1,6 @@
 use std::usize;
 
-use super::{
-    components::{Sprite, Transform, Velocity},
-    entity::Entity,
-};
+use super::entity::Entity;
 
 pub mod signatures {
     pub const TRANSFORM: u8 = 0b0000_0001;
@@ -27,33 +24,5 @@ impl<T> ComponentArray<T> {
             self.components.resize_with(entity as usize + 1, || None);
         }
         self.components[entity as usize] = Some(component);
-    }
-}
-
-pub struct ComponentManager {
-    pub transform_components: ComponentArray<Transform>,
-    pub sprite_components: ComponentArray<Sprite>,
-    pub velocity_components: ComponentArray<Velocity>,
-}
-
-impl ComponentManager {
-    pub fn new() -> Self {
-        ComponentManager {
-            transform_components: ComponentArray::new(),
-            sprite_components: ComponentArray::new(),
-            velocity_components: ComponentArray::new(),
-        }
-    }
-
-    pub fn add_sprite(&mut self, entity: Entity, sprite: Sprite) {
-        self.sprite_components.insert(entity, sprite);
-    }
-
-    pub fn add_transform(&mut self, entity: Entity, transform: Transform) {
-        self.transform_components.insert(entity, transform);
-    }
-
-    pub fn add_velocity(&mut self, entity: Entity, velocity: Velocity) {
-        self.velocity_components.insert(entity, velocity);
     }
 }
--- src/ecs/manager.rs
+++ src/ecs/manager.rs
@@ -3,22 +3,29 @@ use winit::event::KeyEvent;
 use crate::context::Context;
 
 use super::{
-    component::signatures::{SPRITE, TRANSFORM},
-    components::{Sprite, Transform},
-    systems::{Input, Render},
-    ComponentManager, Entity, EntityManager,
+    component::{
+        signatures::{SPRITE, TRANSFORM, VELOCITY},
+        ComponentArray,
+    },
+    components::{Sprite, Transform, Velocity},
+    systems::{movement::Movement, Input, Render},
+    Entity, EntityManager,
 };
 
 pub struct Manager {
     entity_manager: EntityManager,
-    component_manager: ComponentManager,
+    transform_components: ComponentArray<Transform>,
+    sprite_components: ComponentArray<Sprite>,
+    velocity_components: ComponentArray<Velocity>,
 }
 
 impl Manager {
     pub fn new() -> Self {
         Manager {
             entity_manager: EntityManager::new(),
-            component_manager: ComponentManager::new(),
+            transform_components: ComponentArray::new(),
+            sprite_components: ComponentArray::new(),
+            velocity_components: ComponentArray::new(),
         }
     }
 
@@ -27,17 +34,37 @@ impl Manager {
     }
 
     pub fn add_sprite(&mut self, entity: Entity, sprite: Sprite) {
-        self.component_manager.add_sprite(entity, sprite);
+        self.sprite_components.insert(entity, sprite);
         let mut signature = self.entity_manager.get_signature(entity);
         signature |= 1 << SPRITE;
         self.entity_manager.set_signature(entity, signature);
     }
 
     pub fn add_transform(&mut self, entity: Entity, transform: Transform) {
-        self.component_manager.add_transform(entity, transform);
+        self.transform_components.insert(entity, transform);
         let mut signature = self.entity_manager.get_signature(entity);
         signature |= 1 << TRANSFORM;
         self.entity_manager.set_signature(entity, signature);
+    }
+
+    pub fn add_velocity(&mut self, entity: Entity, velocity: Velocity) {
+        self.velocity_components.insert(entity, velocity);
+        let mut signature = self.entity_manager.get_signature(entity);
+        signature |= 1 << VELOCITY;
+        self.entity_manager.set_signature(entity, signature);
+    }
+
+    pub fn handle_input(&self, key_event: KeyEvent) {
+        Input::handle_key_event(key_event);
+    }
+
+    pub fn update(&self, delta_time: f32) {
+        Movement::update(
+            delta_time,
+            &mut self.component_manager.transform_components.components,
+            &self.component_manager.velocity_components.components,
+            &self.entity_manager.entity_signatures,
+        );
     }
 
     pub fn render(&self, context: &Context) -> Result<(), wgpu::SurfaceError> {
@@ -46,9 +73,5 @@ impl Manager {
             &self.component_manager.sprite_components.components,
             &self.entity_manager.entity_signatures,
         );
-    }
-
-    pub fn handle_input(&self, key_event: KeyEvent) {
-        Input::handle_key_event(key_event);
     }
 }
