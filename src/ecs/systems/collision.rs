0c0f578ea85fc9b5af0dac31bbf1c077d55f0ea0
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2024-12-16 16:40:21 +0100

    fixed logic in is colliding

--- src/ecs/systems/collision.rs
+++ src/ecs/systems/collision.rs
@@ -19,11 +19,15 @@ impl Collision {
         for (i, signature) in entity_signatures.iter().enumerate() {
             if (*signature & Collision::SIGNATURE) == Collision::SIGNATURE {
                 if let Some(collider_a) = &colliders[i] {
+                    println!("{:?}", collider_a.layer);
                     for (j, collider_b) in colliders.iter().enumerate().skip(i + 1) {
                         if let Some(collider_b) = collider_b {
+                            println!("{:?}", collider_b.layer);
                             if Self::can_collide(collider_a, collider_b) {
                                 if let Some(transform_a) = &transforms[i] {
+                                    println!("{:?}", transform_a.position);
                                     if let Some(transform_b) = &transforms[j] {
+                                        println!("{:?}", transform_b.position);
                                         if Self::is_colliding(transform_a, transform_b) {
                                             println!("Colliding");
                                         }
@@ -43,9 +47,9 @@ impl Collision {
     }
 
     fn is_colliding(transform_a: &Transform, transform_b: &Transform) -> bool {
-        let x_overlap = (transform_a.position[0] - (transform_b.position[0]).abs())
+        let x_overlap = (transform_a.position[0] - transform_b.position[0]).abs()
             <= (transform_a.scale[0] + transform_b.scale[0] / 2.0);
-        let y_overlap = (transform_a.position[1] - (transform_b.position[1]).abs())
+        let y_overlap = (transform_a.position[1] - transform_b.position[1]).abs()
             <= (transform_a.scale[1] + transform_b.scale[1] / 2.0);
 
         x_overlap && y_overlap
